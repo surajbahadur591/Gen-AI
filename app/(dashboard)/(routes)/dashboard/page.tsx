624d3135457cff36ffbd7753a721a03a4e8b5ee5
Suraj Bahadur <surajbahadur58@gmail.com>
2024-02-04 20:41:45 +0530

    dashboard screen

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,35 @@
+"use client"
+import { useRouter } from "next/navigation";
+import { tools } from "@/constants";
+import { Card } from "@/components/ui/card";
+import { ArrowRight } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 const Dashboard = () => {
+    const router = useRouter();
+
     return (
         <div>
-            dashboard (protected)
+            <div className="mb-8 space-y-4">
+                <h2 className="text-2xl md:text-4xl font-bold text-center">
+                    Explore the power of Gen AI
+                </h2>
+            </div>
+            <div className="px-4 md:px-20 lg:px-32 space-y-4">
+                {tools.map((tool) => (
+                    <Card onClick={() => router.push(tool.href)} key={tool.href} className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer">
+                        <div className="flex items-center gap-x-4">
+                            <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                                <tool.icon className={cn("w-8 h-8", tool.color)} />
+                            </div>
+                            <div className="font-semibold">
+                                {tool.label}
+                            </div>
+                        </div>
+                        <ArrowRight className="w-5 h-5" />
+                    </Card>
+                ))}
+            </div>
         </div>
     );
 }
