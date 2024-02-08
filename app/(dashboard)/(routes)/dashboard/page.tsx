beed8af9f2988262d8f5f071d6794b55cb650a93
Suraj Bahadur <surajbahadur58@gmail.com>
2024-02-09 00:32:05 +0530

    pushing changes

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -9,6 +9,7 @@ const Dashboard = () => {
     const router = useRouter();
 
     return (
+
         <div>
             <div className="mb-8 space-y-4">
                 <h2 className="text-2xl md:text-4xl font-bold text-center">
