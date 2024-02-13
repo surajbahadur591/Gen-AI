21a717f0f0cefa6ce8516231206bc092ee1a614f
Suraj Bahadur <surajbahadur58@gmail.com>
2024-02-13 22:29:40 +0530

    stripe code

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,73 @@
+import { NextResponse } from 'next/server';
+import Stripe from 'stripe'
+import { headers } from 'next/headers'
+
+import prismadb from "@/lib/prismadb";
+import { stripe } from "@/lib/stripe";
+
+export async function POST(req: Request) {
+
+    const body = await req.text()
+
+    const signature = headers().get("Stripe-Signature") as string;
+
+    let event: Stripe.Event;
+
+    try {
+
+        event = stripe.webhooks.constructEvent(
+            body,
+            signature,
+            process.env.STRIPE_WEBHOOK_SECRET!
+        )
+    } catch (error) {
+
+        console.log("[STRIPE WEBHOOK ERROR]", { status: 500 })
+        return new NextResponse("[STRIPE WEBHOOK ERROR]", { status: 400 })
+    }
+
+
+    const session = event.data.object as Stripe.Checkout.Session;
+    let subscription;
+    if (event.type === "checkout.session.completed") {
+        subscription = await stripe.subscriptions.retrieve(
+            session.subscription as string
+        )
+    }
+
+    if (!session?.metadata?.userId) {
+        return new NextResponse("User Id is required", { status: 400 })
+    }
+
+    await prismadb.userSubscription.create({
+        data: {
+            userId: session?.metadata?.userId,
+            stripeSubscriptionId: subscription?.id,
+            stripeCustomerId: subscription?.customer as string,
+            stripePriceId: subscription?.items.data[0].price.id,
+            stripeCurrentPeriodEnd: new Date(
+                subscription?.current_period_end ? subscription.current_period_end * 1000 : new Date()
+            )
+        }
+    })
+
+    if (event.type === 'invoice.payment_succeeded') {
+        const subscription = await stripe.subscriptions.retrieve(
+            session.subscription as string
+        )
+        await prismadb.userSubscription.update({
+            where: {
+                stripeSubscriptionId: subscription?.id,
+            },
+            data: {
+                stripePriceId: subscription?.items.data[0].price.id,
+                stripeCurrentPeriodEnd: new Date(
+                    subscription?.current_period_end ? subscription.current_period_end * 1000 : new Date()
+                )
+            }
+        })
+
+    }
+
+    return new NextResponse(null, { status: 200 })
+}
--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -4,7 +4,7 @@ declare global {
     var prisma: PrismaClient | undefined;
 };
 
-const prismadb = globalThis.prisma || new PrismaClient();
+const prismadb = globalThis.prisma ?? new PrismaClient();
 
 if (process.env.ENVIRONMENT !== 'production') globalThis.prisma = prismadb;
 
